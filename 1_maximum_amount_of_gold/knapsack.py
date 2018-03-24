38d513f46d0c71801d200f9a2299542b0d24f98e
FaiBarazi <f.barazi@gmail.com>
2018-03-24 19:19:20 -0400

    implement list instead of hash table for memory usage

--- 1_maximum_amount_of_gold/knapsack.py
+++ 1_maximum_amount_of_gold/knapsack.py
@@ -1,26 +1,22 @@
 # Uses python3
 import sys
 
+
 def optimal_weight(W, w):
     """Solve discrete Knapsack problem without repetition."""
-    table = {}
     items_size = len(w) + 1
     weight_size = W + 1
     # pad first column with zeros
-    for i in range(items_size):
-        table[0, i] = 0
-    # pad first row with zeros
-    for i in range(weight_size):
-        table[i, 0] = 0
+    table = [[0 for i in range(weight_size)] for j in range(items_size)]
     for i in range(1, items_size):
         for j in range(1, weight_size):
-            table[j, i] = table[j, i - 1]
+            table[i][j] = table[i - 1][j]
             ith_value = w[i - 1]
             if ith_value <= j:
-                value = table[j - ith_value, i - 1] + ith_value
-                if table[j, i] < value:
-                    table[j, i] = value
-    return table[W, items_size - 1]
+                value = table[i - 1][j - ith_value] + ith_value
+                if table[i][j] < value:
+                    table[i][j] = value
+    return table[items_size - 1][W]
 
 
 if __name__ == '__main__':
